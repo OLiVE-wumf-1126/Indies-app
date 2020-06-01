9bc5d8659c8f2c09ae18976abdc66b5d2c6c4c10
OLiVE-wumf-1126 <63233287+OLiVE-wumf-1126@users.noreply.github.com>
2020-06-02 01:28:52 +0900

    編集削除機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,26 @@ class PostsController < ApplicationController
 
   def create
     Post.create(post_params)
+    redirect_to root_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.delete
+    redirect_to root_path
   end
 
   
