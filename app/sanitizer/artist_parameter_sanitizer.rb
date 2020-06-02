70a21b2261d3b10b100700b1c0caf519bc6f4785
OLiVE-wumf-1126 <63233287+OLiVE-wumf-1126@users.noreply.github.com>
2020-06-02 23:13:53 +0900

    投稿要素調整

--- app/sanitizer/artist_parameter_sanitizer.rb
+++ app/sanitizer/artist_parameter_sanitizer.rb
@@ -2,7 +2,6 @@ class Artist::ParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
     permit(:sign_up, keys: [:artistname, :email, ])
-    permit(:sign_in, keys: [:artistname, :email, ])
     permit(:account_update, keys: [:artistname, :email, :artistimage, :profile])
   end
 end
