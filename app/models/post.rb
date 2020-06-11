8be97f0c476cea0b7ff1d4c086aa131d4226989b
OLiVE-wumf-1126 <63233287+OLiVE-wumf-1126@users.noreply.github.com>
2020-06-11 22:36:00 +0900

    commentモデル作成、アソシエーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :artist
   has_many :favorites
+  has_many :comments
+
 
 
   def favorited_by?(listener)
     favorites.where(listener_id: listener.id).exists?
-end
+  end
 end
