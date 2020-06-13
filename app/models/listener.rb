ff96197957dd920bde65001dd2f43e9e59f718a7
OLiVE-wumf-1126 <63233287+OLiVE-wumf-1126@users.noreply.github.com>
2020-06-13 16:20:55 +0900

    各ユーザーバリデーション設定

--- app/models/listener.rb
+++ app/models/listener.rb
@@ -30,4 +30,10 @@ class Listener < ApplicationRecord
   def following?(other_listener)
     self.followings.include?(other_listener)
   end
+
+  before_save { self.email = email.downcase }
+  VALID_PASSWORD_REGEX =       /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,100}\z/i
+
+  validates :nickname,          presence: true
+  validates :password, :password_confirmation,          allow_blank: true, format: { with: VALID_PASSWORD_REGEX }
 end
