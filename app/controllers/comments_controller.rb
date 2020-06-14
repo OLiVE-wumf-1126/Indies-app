0188b29fc819009d3ceaea41559bef945a706d80
OLiVE-wumf-1126 <63233287+OLiVE-wumf-1126@users.noreply.github.com>
2020-06-14 13:00:58 +0900

    commentsコントローラのbefore_action設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :listener_action, only:[:create]
   def create
     comment = Comment.create(comment_params)
     redirect_back(fallback_location: root_path)
@@ -7,5 +8,9 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:comment).merge(listener_id: current_listener.id, post_id: params[:post_id])
+    end
+    
+    def listener_action
+      redirect_to root_path unless listener_signed_in?
     end
 end
