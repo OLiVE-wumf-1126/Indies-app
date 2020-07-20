4d0e89a22a908ce70c415d6e69aa6f596fd40a4f
OLiVE-wumf-1126 <63233287+OLiVE-wumf-1126@users.noreply.github.com>
2020-07-21 01:43:32 +0900

    デプロイ設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
-server "54.249.239.230", user: "ec2-user", roles: %w{app db web}
+server "54.168.251.188/", user: "kondooo", roles: %w{app db web}
 #serverのIPと、ログイン可能なuser名を記載してください
 
 
 set :ssh_options, {
-  keys: %w(~/.ssh/indies-app.pem), #秘密キーのpathを記載
+  keys: %w(~/.ssh/kondooo_key_rsa), #秘密キーのpathを記載
   forward_agent: true,
   auth_methods: %w(publickey),
   port: 22
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,42 +1,31 @@
-APP_PATH   = "#{File.dirname(__FILE__)}/.." unless defined?(APP_PATH)
-RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
-RAILS_ENV  = ENV['RAILS_ENV'] || 'development'
-
-worker_processes 3
-
-listen "/tmp/unicorn.sock"
-pid "tmp/pids/unicorn.pid"
-
-preload_app true
-
-timeout 60
-working_directory APP_PATH
-
-# logのpath
-stderr_path "#{RAILS_ROOT}/log/unicorn_error.log"
-stdout_path "#{RAILS_ROOT}/log/unicorn_access.log"
-
-if GC.respond_to?(:copy_on_write_friendly=)
-  GC.copy_on_write_friendly = true
-end
-
-before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = APP_PATH + "/Gemfile"
-end
-
-before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-
-  old_pid = "#{ server.config[:pid] }.oldbin"
-  unless old_pid == server.pid
-    begin
-      Process.kill :QUIT, File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
-
+$worker  = 2
+  $timeout = 30
+  $app_dir = "/var/www/rails/indies-app" #自分のアプリケーション名
+  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+  $std_log = File.expand_path 'log/unicorn.log', $app_dir
+  # set config
+  worker_processes  $worker
+  working_directory $app_dir
+  stderr_path $std_log
+  stdout_path $std_log
+  timeout $timeout
+  listen  $listen
+  pid $pid
+  # loading booster
+  preload_app true
+  # before starting processes
+  before_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+    old_pid = "#{server.config[:pid]}.oldbin"
+    if old_pid != server.pid
+      begin
+        Process.kill "QUIT", File.read(old_pid).to_i
+      rescue Errno::ENOENT, Errno::ESRCH
+      end
     end
   end
-end
-
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-end
+  # after finishing processes
+  after_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  end
