2d8c7f55351692fb82dbaf5be914f9779b55ce33
OLiVE-wumf-1126 <63233287+OLiVE-wumf-1126@users.noreply.github.com>
2020-07-23 07:56:04 +0900

    capistrano設定変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,10 @@
-server "kondooo_key_rsa"
+server "54.168.251.188", user: "kondooo", roles: %w{app db web}
+#serverのIPと、ログイン可能なuser名を記載してください
+
+
+set :ssh_options, {
+  keys: %w(~/.ssh/kondooo.pem), #秘密キーのpathを記載
+  forward_agent: true,
+  auth_methods: %w(publickey),
+  port: 22
+}
